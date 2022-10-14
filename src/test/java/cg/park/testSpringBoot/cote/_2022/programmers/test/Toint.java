c22c683ec832585919ff23743f08e52290d90b79
pcg0902 <qkrcksrbs8@naver.com>
2022-10-14 16:31:58 +0900

    [프로그래머스] 숫자 변환

---
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/programmers/test/Toint.java
@@ -0,0 +1,17 @@
+package cg.park.testSpringBoot.cote._2022.programmers.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Toint {
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    @Test
+    public void print() {
+
+    }
+}
