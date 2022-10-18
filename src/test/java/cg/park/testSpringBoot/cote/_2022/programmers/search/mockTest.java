b9467c570d17e6a4f057cafae9c6906ee9ed5c9a
pcg0902 <qkrcksrbs8@naver.com>
2022-10-18 09:28:44 +0900

    [프로그래머스] 수포자 정답지

---
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/programmers/search/mockTest.java
@@ -0,0 +1,50 @@
+package cg.park.testSpringBoot.cote._2022.programmers.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class mockTest {
+
+    public List<Integer> maxIntegers (int max, int[] cnt) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < cnt.length; i++) {
+            if(max == cnt[i]) list.add(i+1);
+        }
+        return list;
+    }
+
+    public int maxScore(int[] cnt) {
+        return Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
+    }
+
+    public int[] solution(int[] answers) {
+
+        // 1. 답안지 작성 패턴
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        // 2. 각 수포자 정답 수 계산
+        int[] cnt = new int[3];
+        for(int i=0; i < answers.length; i++){
+            if(answers[i] == person1[i%5]) cnt[0]++;
+            if(answers[i] == person2[i%8]) cnt[1]++;
+            if(answers[i] == person3[i%10]) cnt[2]++;
+        }
+
+        // 3. 최고 득점자 출력
+        return maxIntegers(maxScore(cnt), cnt)
+                .stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    @Test
+    public void print() {
+
+    }
+}
