784632b419db3b3e0292c4e7b8ef83baccbf0e6c
pcg0902 <qkrcksrbs8@naver.com>
2022-11-17 09:23:08 +0900

    [프로그래머스] 폰캣몬

---
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/kakao2020/pv.java
@@ -0,0 +1,22 @@
+package cg.park.testSpringBoot.cote._2022.kakao2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class pv {
+
+    @Test
+    public void test() {
+        String p = "";
+        System.out.println(solution(p));
+    }
+
+    public String solution(String p) {
+        if (p == null || "".equals(p)) return "";
+
+        String answer = "";
+        return answer;
+    }
+
+}
