82a25ce789a43d476d76733e120ae11befe2b31c
pcg0902 <qkrcksrbs8@naver.com>
2022-11-17 09:22:10 +0900

    [프로그래머스] 폰캣몬

--- src/test/java/cg/park/testSpringBoot/cote/_2022/programmers/hash/Phoneketmon.java
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/programmers/hash/Phoneketmon.java
@@ -9,13 +9,6 @@ import java.util.stream.Collectors;
 @SpringBootTest
 public class Phoneketmon {
 
-
-    // 스트림을 사용.
-    // boxed(): int -> Integer, double -> Double
-    // collect: 데이터 타입 지정.
-    // Collectors.toSet().size(): set으로 중복 제거 후 Count
-    // (nums.length / 2): 입력 받은 폰켓몬 수 절반
-    // Integer.min 으로 둘 중 작은 수를 반환.
     public int solution(int[] nums) {
         return Integer.min(
                 Arrays.stream(nums)
