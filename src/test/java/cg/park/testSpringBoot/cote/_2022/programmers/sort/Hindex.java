9f929d5b870a06cc9acbd1b80136cd63c9120fd6
pcg0902 <qkrcksrbs8@naver.com>
2022-11-21 15:01:17 +0900

    [프로그래머스] H인덱스

---
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/programmers/sort/Hindex.java
@@ -0,0 +1,22 @@
+package cg.park.testSpringBoot.cote._2022.programmers.sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Hindex {
+
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    @Test
+    public void print() {
+
+        int[] citations = {3, 0, 6, 1, 5};
+
+        System.out.println(solution(citations));
+    }
+}
