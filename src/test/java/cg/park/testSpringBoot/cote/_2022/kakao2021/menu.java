1a92cb0ef9d1e6db061b529569eb6656aa544b53
pcg0902 <qkrcksrbs8@naver.com>
2023-02-20 10:42:00 +0900

    메뉴 구성 구현

---
+++ src/test/java/cg/park/testSpringBoot/cote/_2022/kakao2021/menu.java
@@ -0,0 +1,20 @@
+package cg.park.testSpringBoot.cote._2022.kakao2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+
+@SpringBootTest
+public class menu {
+
+    @Test
+    public void test() {
+        HashMap map = new HashMap();
+
+    }
+
+
+
+
+}
