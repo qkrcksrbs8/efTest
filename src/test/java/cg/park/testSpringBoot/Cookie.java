f6d85a4b7853d19da4321c5f8bb73559e4514c65
pcg0902 <qkrcksrbs8@naver.com>
2023-02-27 09:52:24 +0900

    쿠키 설정 테스트

---
+++ src/test/java/cg/park/testSpringBoot/Cookie.java
@@ -0,0 +1,15 @@
+package cg.park.testSpringBoot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Cookie {
+    private int x;
+    private int y;
+    private int speed;
+    private int size;
+
+
+}
