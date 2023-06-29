c1c3655e5205de689fda9b1252ba87159e7a15df
pcg0902 <qkrcksrbs8@naver.com>
2023-06-29 09:11:31 +0900

    비동기 테스트 실행부분

---
+++ src/test/java/cg/park/testSpringBoot/AsyncTest.java
@@ -0,0 +1,26 @@
+package cg.park.testSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class AsyncTest {
+
+    @Test
+    public void test() {
+        Executor executor = Executors.newFixedThreadPool(30);
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(10L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ": hi");
+        }, executor);
+    }
+}
