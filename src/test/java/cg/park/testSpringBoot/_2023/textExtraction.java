1998792aa12f028adf26589db4f0366c8d7935b4
pcg0902 <qkrcksrbs8@naver.com>
2023-07-13 10:04:24 +0900

    문자열 contains 리팩터링

--- src/test/java/cg/park/testSpringBoot/_2023/textExtraction.java
+++ src/test/java/cg/park/testSpringBoot/_2023/textExtraction.java
@@ -10,11 +10,19 @@ public class textExtraction {
     public void test() {
         String url = "https://api.happysync.io/api/v1/oauth/kakao/63b177528f502a707b8b5b3e";
 
-        if (url.contains("happysync") && url.contains("kakao")) {
+        if (contains(url, "happysync", "kakao")) {
             System.out.println("save!!!!!!");
         }
 
         System.out.println("end");
+    }
+
+    public boolean contains(String str, String check) {
+        return str.contains(check);
+    }
+
+    public boolean contains(String str, String check1, String check2) {
+        return str.contains(check1) && str.contains(check2);
     }
 
 }
