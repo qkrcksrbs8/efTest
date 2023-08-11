ff6b2638f51e6b5dc1b7ccc5221f041b5fba520b
pcg0902 <qkrcksrbs8@naver.com>
2023-08-11 13:44:27 +0900

    날짜 범위 체크 YYYYMMDD

---
+++ src/test/java/cg/park/testSpringBoot/_2023/daysCheck.java
@@ -0,0 +1,24 @@
+package cg.park.testSpringBoot._2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class daysCheck {
+
+    @Test
+    public void test() {
+        String startDate = "20230809";
+        String endDate = "20230831";
+        String today = "20230809";
+
+        if (startDate.compareTo(today) <= 0 && endDate.compareTo(today) >= 0) {
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+
+
+
+    }
+
+}
