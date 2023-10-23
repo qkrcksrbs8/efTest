18692c1bb3e1feb06e9ca7212578d28c0322987d
pcg0902 <qkrcksrbs8@naver.com>
2023-10-23 15:42:32 +0900

    time check case add

--- src/test/java/cg/park/testSpringBoot/_2023/TimeCheck.java
+++ src/test/java/cg/park/testSpringBoot/_2023/TimeCheck.java
@@ -12,13 +12,50 @@ public class TimeCheck {
 
     @Test
     public void test () {
-        SimpleDateFormat tmpFormat = new SimpleDateFormat("yyyyMMddHHmm", Locale.KOREA);
-        String fDate = tmpFormat.format(new Date());
-        String startDate = "202310191603";
 
-        System.out.println("fDate: "+fDate);
-        if (startDate.compareTo(fDate) <= 0) {
-            System.out.println("tmpFormat: "+ fDate);
+        test2();
+//        SimpleDateFormat tmpFormat = new SimpleDateFormat("yyyyMMddHHmm", Locale.KOREA);
+//        String fDate = tmpFormat.format(new Date());
+//        String startDate = "202311010330";
+//
+//        System.out.println("fDate: "+fDate);
+//        if (startDate.compareTo(fDate) <= 0) {
+//            System.out.println("tmpFormat: "+ fDate);
+//        }
+
+    }
+
+
+    public void test2() {
+        SimpleDateFormat todatCheck = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+        String fDate = todatCheck.format(new Date());
+        int dc = "20231022".compareTo(fDate);
+        System.out.println("dday: "+dc);
+
+//        if (dc >= 0) {
+//            System.out.println("0000");
+//        }
+//
+//        if (dc >= 1) {
+//            System.out.println("1111");
+//        }
+//
+//        if (dc >= 2) {
+//            System.out.println("22222");
+//        }
+//        if (dc >= 3) {
+//            System.out.println("3333");
+//        }
+//        if (dc >= 4) {
+//            System.out.println("4444");
+//        }
+
+        int cnt = dc > 3 ? 3 : dc;
+
+        if (cnt >= 0) {
+            System.out.println("###: "+0);
+            for (int i = 1; i <= cnt; i++)
+                System.out.println("###: "+i);
         }
 
     }
