f42235e64389f944e271d994c230f1be9a78035d
pcg0902 <qkrcksrbs8@naver.com>
2023-11-01 17:40:14 +0900

    IP 체크 테스트

---
+++ src/test/java/cg/park/testSpringBoot/_2023/IpCheck.java
@@ -0,0 +1,14 @@
+package cg.park.testSpringBoot._2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IpCheck {
+
+    @Test
+    public void test() {
+
+
+    }
+}
