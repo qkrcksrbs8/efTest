46eba8ddcbddb55d920ea3c2fca8e4c1f0e18897
pcg0902 <qkrcksrbs8@naver.com>
2023-11-07 15:27:02 +0900

    IP 체크 테스트

--- src/test/java/cg/park/testSpringBoot/_2023/IpCheck.java
+++ src/test/java/cg/park/testSpringBoot/_2023/IpCheck.java
@@ -9,6 +9,23 @@ public class IpCheck {
     @Test
     public void test() {
 
+//        162.221.144.0	162.221.147.255
+//        167.88.52.0	167.88.55.255
+//        192.190.19.0	192.190.19.255
+//        216.38.167.0	216.38.167.255
+//        64.40.4.0	64.40.7.255
+//        64.66.32.0	64.66.35.255
+
+        String ip = "10.40.137.243";
+
+        System.out.println(ip.substring(0, ip.lastIndexOf(".")));
 
     }
+
+    public boolean isUCM(String ip) {
+        // 미국, 캐나다, 맥시코 IP 체크 함수
+        //
+        return false;
+    }
+
 }
