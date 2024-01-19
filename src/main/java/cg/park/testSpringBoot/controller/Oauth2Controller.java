e7f1ea19959db06200017c02d9cc12c42a62908a
pcg0902 <qkrcksrbs8@naver.com>
2024-01-19 14:36:57 +0900

    oauth2 분기처리 확인

--- src/main/java/cg/park/testSpringBoot/controller/Oauth2Controller.java
+++ src/main/java/cg/park/testSpringBoot/controller/Oauth2Controller.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RequestMapping("/oauth2")
+@RequestMapping("/oauth2")// 각 플랫폼별로 분기 필요.
 @Controller
 public class Oauth2Controller {
 
     @GetMapping("/main")
     public String view() {
-
         return "/oauth2/view";
     }
 
