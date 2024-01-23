779bb768937166fb515defaa3e41c04eab1864e1
pcg0902 <qkrcksrbs8@naver.com>
2024-01-23 13:46:59 +0900

    finaly 테스트

---
+++ src/test/java/cg/park/testSpringBoot/_2024/finallyTest.java
@@ -0,0 +1,34 @@
+package cg.park.testSpringBoot._2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class finallyTest {
+
+    @Test
+    public void test() {
+
+        check();
+
+    }
+
+    public void check() {
+        String str = "null";
+        try {
+            System.out.println("try start");
+            if (1==1) {
+                str = "if 1==1";
+                return;
+            }
+            System.out.println("try end");
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: "+e);
+        }
+        finally {
+            System.out.println("finally !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! str: "+str);
+        }
+        System.out.println("check end !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
