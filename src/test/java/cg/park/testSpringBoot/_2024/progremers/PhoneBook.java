1b5947c45bb943fac8ce1c59024478f72cf3b298
pcg0902 <qkrcksrbs8@naver.com>
2024-05-23 09:26:34 +0900

    [프로그래머스] PhoneBook

---
+++ src/test/java/cg/park/testSpringBoot/_2024/progremers/PhoneBook.java
@@ -0,0 +1,30 @@
+package cg.park.testSpringBoot._2024.progremers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+
+@SpringBootTest
+public class PhoneBook {
+
+    @Test
+    public void test() {
+
+        System.out.println(solution(new String[]{"119", "97674223", "1195524421"}));
+    }
+
+    public boolean solution(String[] phone_book) {
+        HashMap<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < phone_book.length; i++)
+            map.put(phone_book[i], "");
+
+        for (String pb : phone_book)
+            for (int i = 1; i < pb.length(); i++)
+                if (map.containsKey(pb.substring(0, i)))
+                    return false;
+
+        return true;
+    }
+}
